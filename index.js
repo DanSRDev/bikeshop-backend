1076ea6df572ad45a0a598a6e5fada1ffabb49e5
Daniel <dansanrjsdev@hotmail.com>
2023-01-08 11:08:11 -0500

    Express connection

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hola mundo');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json);
+
+app.get('/', (req,res) => {
+  res.send('Bikeshop Backend Home');
+})
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+})
