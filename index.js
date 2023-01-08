26351465428a389b18c96cdd007119db92ae3dd1
Daniel <dansanrjsdev@hotmail.com>
2023-01-08 13:58:56 -0500

    Middleware para errores

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErrors, boomErrorHandler, errorHandler } = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3000;
 
@@ -11,6 +13,10 @@ app.get('/', (req,res) => {
 })
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
