8e327cace5eb95d263b763cb4d3ef45c8c34f564
Daniel <dansanrjsdev@hotmail.com>
2023-01-10 01:41:34 -0500

    cors

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const routerApi = require('./routes');
 
 const { logErrors, boomErrorHandler, errorHandler } = require('./middlewares/error.handler');
@@ -7,6 +9,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://127.0.0.1:5500'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 app.get('/', (req,res) => {
   res.send('Bikeshop Backend Home');
